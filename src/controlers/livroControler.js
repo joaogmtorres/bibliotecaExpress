446e3e3b45193de9165b855f1c6af4be50f01782
joaogmtorres <joaogabrieltorres@gmail.com>
2025-04-10 13:03:24 -0300

    atualização do livroControler e CRUD pelo moongose

--- src/controlers/livroControler.js
+++ src/controlers/livroControler.js
@@ -3,9 +3,14 @@ import livro from "../models/livro.js";
 class LivroControler {
 
     static async listarLivros (req, res) {
-        const listaLivros = await livro.find({});
-        res.status(200).json(listaLivros);
+        try{
+            const listaLivros = await livro.find({});
+            res.status(200).json(listaLivros);
+        }catch{
+            res.status(500).json({ message : `${erro.message} - falha na requisição`})
+        }
     };
+            
 
     static async cadastrarLivro (req, res) {
         try { 
@@ -17,6 +22,37 @@ class LivroControler {
         }
     }
 
+    
+    static async listarLivrosPorId (req, res) {
+        try{
+            const id = req.params.id
+            const livroEncontrado = await livro.findById(id);
+            res.status(200).json(livroEncontrado);
+        }catch{
+            res.status(500).json({ message : `${erro.message} - falha na requisição do livro`})
+        }
+    };
+
+    static async atualizarLivro (req, res) {
+        try{
+            const id = req.params.id
+            await livro.findByIdAndUpdate(id, req.body);
+            res.status(201).json({ message : "atualizado com sucesso"})
+        }catch{
+            res.status(500).json({ message : `${erro.message} - falha na requisição da atualização do livro`})
+        }
+    };
+
+    static async apagarLivro (req, res) {
+        try{
+            const id = req.params.id
+            await livro.findByIdAndDelete(id);
+            res.status(201).json({ message : "atualizado com sucesso"})
+        }catch{
+            res.status(500).json({ message : `${erro.message} - falha na remoção do livro`})
+        }
+    };
 };
+
 
 export default LivroControler;
